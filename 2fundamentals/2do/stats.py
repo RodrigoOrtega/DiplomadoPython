3e868e3f0b1f6ad481e1d8f9936e211acafdf3a0
RodrigoOrtega <rorrofighter@gmail.com>
2022-05-16 22:50:49 -0500

    mejoras de tareas 1-3

--- 2fundamentals/2do/stats.py
+++ 2fundamentals/2do/stats.py
@@ -3,16 +3,33 @@
 # one of the following stat functions
 #
 
-def sum(lst): pass
+# def sum(lst): pass
 
-def avg(l): pass
+# def avg(l): pass
 
-def min(l): pass
+# def min(l): pass
 
-def max(l): pass
+# def max(l): pass
 
-def range(l): pass
+# def range(l): pass
 
-def std(l): pass
+# def std(l): pass
 
-def mode(l): pass
+numbers = [82,15,16,11,15,19,30,18,50,75,19,48,79,19,30,19,33]
+#El numero mas repetido es el 19
+
+#funcion de moda
+def mode(list):
+    frequency = {}
+
+    for value in list:
+        frequency[value] = frequency.get(value, 0) + 1
+
+    most_frequent = max(frequency.values())
+
+    modes = [key for key, value in frequency.items()
+                      if value == most_frequent]
+
+    return modes[0]
+
+print("La moda de la lista es: " + str(mode(numbers)))
--- 3interpolation/2do/2do.py
+++ 3interpolation/2do/2do.py
@@ -2,7 +2,9 @@
 # please refer to PPT file
 # for exercise
 #
+
 #Ejercicio 1
+#Interpolacion lineal
 d1 = (-4, -2)
 d2 = (1, 5)
 
@@ -11,10 +13,11 @@ def fn(y, d1, d2):
     b = y - d1[1]
     return a * b + d1[0]
 
-print("El resultado de la tarea 1 es:")
+print("El resultado de la primer parte de la tarea es: ")
 print(fn(3.7, d1, d2))
 
 #Ejercicio 2
+#Calculado con el polinomio interpolante de 4 puntos
 d1 = (-2,4)
 d2 = (-1,-2)
 d3 = (3,5)
@@ -27,5 +30,16 @@ def fn(x,d1,d2,d3,d4):
     d = (((x-d1[0])*(x-d2[0])*(x-d3[0]))/((d4[0]-d1[0])*(d4[0]-d2[0])*(d4[0]-d3[0])))*d4[1]
     return a+b+c+d
 
-print("El resultado de la tarea 2 es:")
+print("El resultado de la segunda parte de la tarea es: ")
 print(fn(7.7,d1,d2,d3,d4))
+
+#Ejercicio 3
+#Calculado con el polinomio interpolante de Lagrange, librerías de numpy y scipy
+import numpy as np
+from scipy.interpolate import lagrange
+
+x = np.array([-1, 0, 4, 5])
+
+y = np.array([3, -7, 7, 11])
+
+print("El resultado de la tercer parte de la tarea es: \n" + str(lagrange(x, y)))
