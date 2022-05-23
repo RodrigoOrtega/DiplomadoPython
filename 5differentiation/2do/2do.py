cd928d058336872f35a267f76bae3587cc288cd9
RodrigoOrtega <rorrofighter@gmail.com>
2022-05-22 19:24:14 -0500

    Tarea 5

--- 5differentiation/2do/2do.py
+++ 5differentiation/2do/2do.py
@@ -2,3 +2,12 @@
 # please refer to PPT file
 # for exercise
 #
+
+import numpy as np
+from scipy.misc import derivative
+
+def f(x):
+
+    return (np.sin(2*x)**3)/((x**4)+1) 
+
+print(derivative(f, 2.45, dx=1e-6))
