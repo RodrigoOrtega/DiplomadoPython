db426d9acea83a1a52a080ba9c60207d43b81431
RodrigoOrtega <rorrofighter@gmail.com>
2022-05-22 19:55:33 -0500

    Diferencias progresiva, regresiva y centrada

--- 5differentiation/2do/2do.py
+++ 5differentiation/2do/2do.py
@@ -4,10 +4,19 @@
 #
 
 import numpy as np
-from scipy.misc import derivative
 
+def derivative(f,a,method='central',h=0.01):
+    if method == 'central':
+        return (f(a + h) - f(a - h))/(2*h)
+    elif method == 'forward':
+        return (f(a + h) - f(a))/h
+    elif method == 'backward':
+        return (f(a) - f(a - h))/h
+    else:
+        raise ValueError("Method must be 'central', 'forward' or 'backward'.")
 def f(x):
+    return (np.sin(2*x)**3)/((x**4)+1)
 
-    return (np.sin(2*x)**3)/((x**4)+1) 
-
-print(derivative(f, 2.45, dx=1e-6))
+print("Diferenciacion finita regresiva: " + str(derivative(f,2.54,method='backward',h=1e-15)))
+print("Diferenciacion finita centrada: " + str(derivative(f,2.54,method='central',h=1e-15)))
+print("Diferenciacion finita progresiva: " + str(derivative(f,2.54,method='forward',h=1e-15)))
